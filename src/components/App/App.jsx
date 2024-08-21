419b265e0cf94db727bf978941f2b59ab80921af
visacci <125196398+visacci@users.noreply.github.com>
2024-08-21 10:47:57 +0300

    initial code

---
+++ src/components/App/App.jsx
@@ -0,0 +1,6 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+function App() {
+  return <div className="app">hello world</div>;
+}
+export default App;
