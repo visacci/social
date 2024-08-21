b9b79c49e9e79337087fbf69207bf8ffef0f9c44
visacci <visacciallanz14@gmail.com>
2024-08-21 11:37:23 +0300

    mali added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  return <div className="app">hello world</div>;
+  return <div className="app">hello world on malis branch</div>;
 }
 export default App;
