def4538771226beceb4d56b89ad147cc84b4e402
visacci <visacciallanz14@gmail.com>
2024-08-21 11:32:58 +0300

    allan commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  return <div className="app">hello world</div>;
+  return <div className="app">hello world on allans branch</div>;
 }
 export default App;
